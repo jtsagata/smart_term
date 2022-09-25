9f7f7d447c165533a8300ee084118802052c9f42
Ioannis Tsagkatakis <tsagatakis@protonmail.com>
2022-09-25 23:39:47 +0300

    Always source

--- automation.sh
+++ automation.sh
@@ -1,9 +1,50 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
+# shellcheck shell=bash
+AUTOMATION_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
 
 # Add the following lines to .bashrc
 # # shellcheck source=/dev/null
-# source ~/.automation/automation.sh
+# source ~/.automation/automation.sh load
 
-# shellcheck source=/dev/null
-source "${SCRIPT_DIR}/modules/mod_tool" load
+function path_add() {
+    if ! echo "$PATH" | /bin/grep -Eq "(^|:)$1($|:)"; then
+        if [ "$2" = "after" ]; then
+            PATH="$PATH:$1"
+        else
+            PATH="$1:$PATH"
+        fi
+    fi
+}
+
+path_add "${AUTOMATION_DIR}/bin"
+
+### Get the SHELL NAME
+if [ -n "$BASH" ]; then
+    export SHELL_NAME="bash"
+elif [ -n "$ZSH_NAME" ]; then
+    export SHELL_NAME="zsh"
+elif [ -n "$FISH_VERSION" ]; then
+    export SHELL_NAME="fish"
+fi
+
+# All the action is here
+function cmd_load() {
+    # shellcheck source=/dev/null
+    source "${AUTOMATION_DIR}/modules/mod_tool" load
+}
+
+function cmd_modules() {
+    # shellcheck source=/dev/null
+    source "${AUTOMATION_DIR}/modules/mod_tool" "$@"
+}
+
+
+command=${1-help}
+shift || true
+func=cmd_"${command}"
+if [ "$(type -t "$func")" == "function" ]; then
+    $func "$@"
+fi
+
+
+# This is sourced, so remove garbage
+unset AUTOMATION_DIR cmd_load
